48a728219747c556a42b663fb27fad868642047f
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2021-12-05 21:12:58 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env -S bash -euo pipefail
+
+FILES=$(find src -name "*.cpp")
+mkdir -p build
+
+for f in $FILES; do
+	g++ -c -g -O2 -I. $f -o "build/$(basename ${f%.cpp}.o)"
+done
+
+ar rcs lib/libtbc.a build/*.o
+
+# We can also compile a shared library?
+# g++ -g -O2 -fPIC $FILES -I. -shared -o lib/libtbc.so
+
+echo Success
+ls lib
